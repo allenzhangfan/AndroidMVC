72a2031d2aac60cb99c399e58898231273118740
allenzhang <allenzhangfan@gmail.com>
2014-04-17 15:15:33 +0800

    初始导入

--- core/org/railsdroid/core/util/Status.java
+++
@@ -1,8 +0,0 @@
-package org.railsdroid.core.util;
-
-public class Status {
-	public static final int ADD = 250001;
-	public static final int EDIT = 250002;
-	public static final int SHOW = 250003;
-	public static final int SHOW_LIST_FROM_INTENT = 250003;
-}
